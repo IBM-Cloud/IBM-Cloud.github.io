93c51911a090c487226c5b8bcb96087f9df70ba7
Twana Daniel <twanaazi@ie.ibm.com>
2017-02-28 16:35:04 +0000

    set up Tealium/Coremetrics

---
+++ scripts/bluemixAnalytics.js
@@ -0,0 +1,23 @@
+digitalData = {
+
+    page: {
+
+        pageInfo: {
+
+            ibm: {
+
+                siteID: 'SAMPLES9999' // Set this to your SITEID
+
+            }
+
+        },
+
+        category: {
+
+            primaryCategory: 'BluemixSamples' // Set this to your PAGE_CATEGORY
+
+        }
+
+    }
+
+}
