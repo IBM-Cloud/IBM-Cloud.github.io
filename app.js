fc7a64f0e2fcfa78efdc01a7188297cb5866c983
Twana Daniel <twanaazi@ie.ibm.com>
2017-03-13 16:28:09 +0000

    Add animations and routing

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-angular.module('ngSamples', ['ui.bootstrap', 'ngCookies', 'angulartics', 'angulartics.segment']);
+// Self executable function that handles the site routes and get's all the external modules
+// ngSamples = Application module
+// ngRoute = handles the routes
+// ui.bootstrap = helps with the bootstrap UI
+// ngCookies = helps with creating cookies
+// ngulartics & angulartics.segment = helps with tracking user clicks on the page that get sent to Segment
+// ngAnimate = for using Angular animations
+(function() {
+    var app = angular.module('ngSamples', ['ngRoute', 'ui.bootstrap', 'ngCookies', 'angulartics', 'angulartics.segment', 'ngAnimate']);
+
+    app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'samplesController',
+                templateUrl: 'app/views/home.html'
+            })
+            .when('/index', {
+                controller: 'samplesController',
+                templateUrl: 'app/views/home.html'
+            })
+            .otherwise( { redirectTo: '/' } );
+    });
+}());
