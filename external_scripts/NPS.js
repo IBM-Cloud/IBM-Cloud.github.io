06b0d87c02be4db9cb34e087bac71dd36c1c9dcd
Twana Daniel <twanaazi@ie.ibm.com>
2017-03-16 15:09:40 +0000

    Remove NPS force

--- external_scripts/NPS.js
+++ external_scripts/NPS.js
@@ -1,7 +1,7 @@
 window.ibmNps = {
     offeringId: "af2c9d18-7f3a-4b6f-bc69-d263e81abf24",
-    //deferSurvey: true,
+    deferSurvey: true
     //userId: "some.user@myoffering.com"
     //test: false
-    forceSurvey: true
+    //forceSurvey: true
 };
