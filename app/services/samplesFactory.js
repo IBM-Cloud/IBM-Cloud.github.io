a80be2f82dccdb1d33d8a087ba356e78a7c592e7
Vidyasagar Machupalli <vidyasagarmsc@gmail.com>
2020-08-27 19:45:33 +0530

    Fixed images and copyright year

--- app/services/samplesFactory.js
+++ app/services/samplesFactory.js
@@ -3,7 +3,7 @@ angular
   .factory('solutionFactory', function($http){
     function getSolutions(){
       // return $http.get('data/tutorials.json');
-      return $http.get('https://raw.githubusercontent.com/IBM-Bluemix-Docs/tutorials/master/tutorials.json');
+      return $http.get('https://raw.githubusercontent.com/ibm-cloud-docs/solution-tutorials/master/tutorials.json');
     }
     return{
       getSolutions: getSolutions
